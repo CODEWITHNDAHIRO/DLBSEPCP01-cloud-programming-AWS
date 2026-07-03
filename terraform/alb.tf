d6ae7475e1d105754c49652688a55d8c55a98958
CODEWITHNDAHIRO <ndahiro.putinejonathan@iu-study.org>
2026-07-03 13:09:07 +0200

    Application Load Balancer - Requesting Routing Components created: - Application Load Balancer (ALB) * which sits in public subnets for internet access and used for security group (HTTP/HTTPS from internet) * Multi-AZ setup for high availability - Target Group * Routes traffic to EC2 instances * Health checks every 30 seconds * Marks instances healthy after 2 successful checks * Marks instances unhealthy after 2 failed checks * Accepts only HTTP 200 responses - Listener * listens on port 80(HTTP) * forwards all traffic to target group -Updated Outputs * ALB DNS name (for accessing load balancer) * ALB ARN (for linking ASG) * Target Group ARN (for linking ASG) All validated with terraform validate.

---
+++ terraform/alb.tf
@@ -0,0 +1,44 @@
+resource "aws_alb" "main"{
+    name          ="iu-alb"
+    internal      = false
+    load_balancer_type = "application"
+    security_groups = [aws_security_group.alb.id]
+    subnets = [aws_subnet.public_az1.id, aws_subnet.public]
+
+    tags = {
+        Name = "iu-alb"
+    }
+    }
+
+resource "aws_lb_target_group" "main" {
+    name = "iu-tg"
+    port = 80
+    protocol = "HTTP"
+    vpc_id = aws_vpc.main.id 
+   
+    health_check {
+        healthy_threshold = 2
+        unhealthy_threshold = 2
+        timeout = 5
+        interval = 30 
+        path = "/"
+        matcher = "200"
+    }
+
+    tags ={
+        Name = "iu-tg"
+    }
+}
+ resource "aws_lb_listener" "http" {
+    load_balancer_arn = aws_lb.main.arn 
+    port  = 80
+    protocol = "HTTP"
+
+    default_action {
+        type  = "forward"
+        target_group_arn = aws_lb_target_group.main.arn 
+    }
+    tags = {
+        Name = "iu-listener-http"
+    }
+ }
